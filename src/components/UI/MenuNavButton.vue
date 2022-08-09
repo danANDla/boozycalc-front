ab19238d6ea09a16cadf7e748aecc9abd7c0d8ad
archdla <d.a.n.202021@gmail.com>
2022-08-09 21:20:23 +0300

    changed color of nav buttons

--- src/components/UI/MenuNavButton.vue
+++ src/components/UI/MenuNavButton.vue
@@ -16,12 +16,12 @@ button {
   display: inline-block;
   width: 100%;
   padding: 0.35em 0.35em;
-  border: 0.1em solid #292929;
+  border: 0.1em solid #3D5E6B;
   box-sizing: border-box;
   text-decoration: none;
   font-size: 20px;
   color: #FFFFFF;
-  background-color: #292929;
+  background-color: #3D5E6B;
   text-align: start;
   transition: all 0.2s;
 }
