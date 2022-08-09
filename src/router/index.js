71a0f6b27ef3cf117417e908a073ab379bcbf4ce
archdla <d.a.n.202021@gmail.com>
2022-08-09 15:47:24 +0300

    added welcome page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,25 @@
 import HomePage from "@/views/HomePage";
 import {createRouter, createWebHistory } from "vue-router";
+import ListsViewer from "@/views/ListsViewer";
+import WelcomePage from "@/views/WelcomePage";
 
 const routes = [
-        {path: '/',  component: HomePage}
+        {
+            path: '/',
+            component: HomePage,
+            children:[
+                {
+                    path: "",
+                    name: "welcome-page",
+                    component: WelcomePage
+                },
+                {
+                    path: "items",
+                    name: "ingredients-and-cocktails",
+                    component: ListsViewer
+                }
+            ]
+        }
 ]
 
 const router = createRouter({
