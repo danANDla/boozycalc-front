cfb170942c4ad3138586338ff939b56bd8672047
archdla <d.a.n.202021@gmail.com>
2022-08-11 00:54:29 +0300

    Add tabz component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,10 +2,12 @@ import button from "@/components/UI/RectButton";
 import menuNavButton from "@/components/UI/MenuNavButton";
 import MyInput from "@/components/UI/MyInput";
 import DialogWindow from "@/components/UI/DialogWindow";
+import MyTabz from "@/components/UI/MyTabz";
 
 export default [
     button,
     menuNavButton,
     MyInput,
     DialogWindow,
+    MyTabz
 ];
