c5b1ac4d94080d7a0fe6949c12e698c57d5e56e4
archdla <d.a.n.202021@gmail.com>
2022-08-11 00:29:01 +0300

    fixed including dialog

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,13 +1,11 @@
 import button from "@/components/UI/RectButton";
 import menuNavButton from "@/components/UI/MenuNavButton";
-import AddItemDialog from "@/components/UI/DialogWindow";
 import MyInput from "@/components/UI/MyInput";
 import DialogWindow from "@/components/UI/DialogWindow";
 
 export default [
     button,
     menuNavButton,
-    AddItemDialog,
     MyInput,
-    DialogWindow
+    DialogWindow,
 ];
