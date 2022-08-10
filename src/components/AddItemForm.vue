d8c2444dffef37e6c30779f8eb137d18763c77ed
archdla <d.a.n.202021@gmail.com>
2022-08-10 23:21:06 +0300

    add diolog window with form

---
+++ src/components/AddItemForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent>
+    <div class="container">
+      <div>
+        <my-input
+            v-model="ingredient.name"
+            type="text"
+            placeholder="item name"
+        />
+      </div>
+      <div>
+        <my-input
+            v-model="ingredient.description"
+            type="text"
+            placeholder="description"
+        />
+      </div>
+      <div class="btn-container">
+        <rect-button btn-type="purple"> add </rect-button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import MyInput from "@/components/UI/MyInput";
+import RectButton from "@/components/UI/RectButton";
+export default {
+  name: "AddItemForm",
+  components: {RectButton, MyInput},
+  data(){
+    return{
+      ingredient:{
+        name: '',
+        description: ''
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+form{
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  background-color: #3D5E6B;
+  justify-content: center;
+}
+.container{
+  width: 100%;
+  display:flex;
+  flex-direction: column;
+}
+.container div{
+  padding: 10px;
+}
+
+.btn-container{
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,13 @@
 import button from "@/components/UI/RectButton";
 import menuNavButton from "@/components/UI/MenuNavButton";
+import AddItemDialog from "@/components/UI/DialogWindow";
+import MyInput from "@/components/UI/MyInput";
+import DialogWindow from "@/components/UI/DialogWindow";
 
 export default [
     button,
     menuNavButton,
+    AddItemDialog,
+    MyInput,
+    DialogWindow
 ];
